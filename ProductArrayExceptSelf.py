4ad717a4e61383d82b2ef01e021d5690b52d25e9
Rishabh <rishabhs2405@gmail.com>
2025-09-14 12:25:45 +0530

    ProductArrayExceptSelf

---
+++ ProductArrayExceptSelf.py
@@ -0,0 +1,23 @@
+"""
+    Problem Statement -> Given an integer array nums, return an array answer where answer[i] is the product of all elements in nums except nums[i].
+    Prefix Product
+    Suffix Product 
+    Return Output Array Working for each element in array
+"""
+
+def product_array(arr):
+    n = len(arr)
+    result = [1]*n
+    prefix = 1
+    for i in range(n):
+        result[i] = prefix
+        prefix *= arr[i]
+    suffix = 1
+    for i in range(n-1,-1,-1):
+        result[i]*= suffix
+        suffix *= arr[i]
+    
+    return result
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+print(f"The Product Array of {arr} is: {product_array(arr)}")
